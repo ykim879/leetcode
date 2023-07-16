acbc10fc8ed72786813d35e02036535e3ab5361c
Alex Kim <59812671+ykim879@users.noreply.github.com>
2023-07-16 11:13:23 -0700

    Create 162.py

---
+++ 162.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return 0
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                return i
+        return len(nums) - 1
