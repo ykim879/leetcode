37d226050231144e5759025c9fc4efabaf79f0a2
Alex Kim <59812671+ykim879@users.noreply.github.com>
2023-09-12 12:12:25 -0700

    Create 223.py

---
+++ 223.py
@@ -0,0 +1,7 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        total = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1)
+        if ax2 <= bx1 or ay2 <= by1 or bx2 <= ax1 or by2 <= ay1:
+            return total 
+        return total - (min(ax2, bx2) - max(ax1, bx1)) * (min(ay2, by2) - max(ay1, by1)) 
+        
