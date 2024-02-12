f2f3405aac059f256fd8e0155505471506936eb6
Alex Kim <59812671+ykim879@users.noreply.github.com>
2024-02-12 13:53:38 -0800

    Create isPalindrome.py

---
+++ isPalindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p1, p2 = 0, len(s) - 1
+        while p1 < p2:
+            if not s[p1].isalnum():
+                p1 += 1
+            elif not s[p2].isalnum():
+                p2 -= 1
+            elif s[p1].lower() != s[p2].lower():
+                return False
+            else:
+                p1 += 1
+                p2 -= 1
+        return True
