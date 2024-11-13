588f261b3ed48201537224fa12964ab0644a09ac
Alex Kim <59812671+ykim879@users.noreply.github.com>
2024-11-12 16:51:57 -0800

    Update moveZeroes.py

--- moveZeroes.py
+++ moveZeroes.py
@@ -4,11 +4,17 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l, r, L = 0, 1, len(nums)
+        # find l value where it is 0
+        while l < L:
+            if nums[l] == 0:
+                break
+            l += 1
+        r = l + 1
         while r < L:
-            if l == r or nums[r] == 0:
-                r += 1
-            elif nums[l] != 0:
-                l += 1
-            else:
+            if nums[r] != 0:
                 nums[l] = nums[r]
-                nums[r] = 0
+                l += 1
+            r += 1
+        while l < L:
+            nums[l] = 0
+            l += 1
